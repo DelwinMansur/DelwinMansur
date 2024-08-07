0b28ce1267e212009e945a3f9eebf6147e962f3a
Delwin Mansur Bawa <bawamansurachebs@gmail.com>
2024-08-07 10:28:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('button-01').addEventListener('click', (event) => {
+    let element_container_01 = document.getElementById('container01');
+    element_container_01.classList.toggle('hidden');
+  });
